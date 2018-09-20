04aedf4d193bfebb7ee5710f6834713d0706b6b1
Vincent <31508339+vbnin@users.noreply.github.com>
2018-09-20 14:01:10 +0200

    Update RaspiClock.py

--- RaspiClock.py
+++ RaspiClock.py
@@ -6,8 +6,7 @@ Développé par IP Echanges - VBNIN et CKAR
 
 """
 
-version_text = 'Version 0.2 - 28/08/2018'
-print('This is RaspiClock - {}').format(version_text)
+version_text = 'Version 0.3 - 20/09/2018'
 
 # Import libraries
 import Tkinter as tk
@@ -17,6 +16,29 @@ import sys
 import subprocess
 import re
 import socket
+
+# Logs files are stored there
+logs_file = os.path.join(sys.path[0],'RaspiClock_logs.log')
+i = 1
+try:
+    while os.path.getsize(logs_file) > 5000000:
+        logs_file = os.path.join(sys.path[0],'RaspiClock_logs_{}.log'.format(str(i)))
+        i += 1
+except:
+    pass
+
+def Log(log):
+    # This function records a message in computer syslogs
+    logline = time.strftime("%d/%m/%Y - %H:%M:%S") + ' : ' + log + '\n'
+    print(logline)
+    try:
+        with open(logs_file, 'a') as l:
+            try:
+                l.writelines(logline.encode("utf-8"))
+            except:
+                l.writelines(logline)
+    except Exception as e:
+        print('Error !! Could not store logs in file.. : {}').format(e)
 
 # Define config file location and name
 ConfigFile = os.path.join(sys.path[0], 'RaspiClock_config.ini')
@@ -30,8 +52,10 @@ class Clock(object):
                 lines = f.read().splitlines()
                 self.Color = lines[0]
                 self.SecondsChoice = lines[1]
+            Log('Config file present in : {}'.format(ConfigFile))
         except Exception as e:
-            print("Cannot read config file : ", e)
+            Log("Cannot read config file : {}".format(e))
+            Log("Using default values for color and seconds")
             self.Color = 'red'
             self.SecondsChoice = 'Oui'
 
@@ -47,12 +71,9 @@ class Clock(object):
             fg='#202020',
             bd=1,
             highlightbackground='black',
-
             highlightcolor='#202020',
             activebackground='#202020')
         self.conf.pack(side='left')
-        
-        
 
         self.net_color='gray'
         self.net_text='Statut Réseau : Inconnu'
@@ -79,6 +100,7 @@ class Clock(object):
     def config(self, event=None):
         # This function allows you to configure the clock
         # Opening popup window
+        Log("User opened config panel")
         root.configure(cursor='arrow')
         self.config_window = tk.Toplevel()
         self.config_window.resizable(False, False)
@@ -109,7 +131,7 @@ class Clock(object):
         self.toggle = tk.OptionMenu(config_frame, self.SecondsToggle, *choix)
         self.toggle.grid(row=1, column=1, sticky='ew', pady=(15,0))
 
-        self.quit = tk.Button(config_frame, text='Quitter le programme', height=1, width=15, command=root.destroy)
+        self.quit = tk.Button(config_frame, text='Quitter le programme', height=1, width=15, command=self.Quit)
         self.quit.grid(row=2, column=0, sticky='w', padx=10, pady=(20,0))
 
         self.about_button = tk.Button(config_frame, text='A propos', height=1, width=8, command=self.about)
@@ -121,14 +143,16 @@ class Clock(object):
         tk.Button(button_frame, text='Retour', height=1, width=6, command=self.click_retour).pack(side='right', padx=10)
 
     def about(self):
-        # This function opens an about window with informations
+        # This function opens a window with 'about' informations
         # Closing Config Window
         self.click_retour()
 
         # Opening popup window
+        Log("User opened 'about' window")
         self.about_window = tk.Toplevel()
         self.about_window.resizable(False, False)
         self.about_window.attributes("-topmost", True)
+        self.config_window.overrideredirect(1)
         self.about_window.title("A propos de RaspiClock")
 
         x = (self.about_window.winfo_screenwidth() - self.about_window.winfo_reqwidth()) / 2
@@ -147,29 +171,44 @@ class Clock(object):
 
     def click_fermer(self):
         # This function closes the about window
+        Log("User closed 'about' window")
         root.configure(cursor='none')
         self.about_window.destroy()
 
     def click_valider(self):
         # This function sets the defined settings anc closes the config window
+        Log("User closed config panel with 'Valider' button")
         if self.ColorChoice.get() != '':
             self.Color = self.ColorChoice.get()
             self.clock.configure(fg=self.Color)
         if self.SecondsToggle.get() != '':
             self.SecondsChoice = self.SecondsToggle.get()
-        with open(ConfigFile, 'w') as f:
-            f.writelines(self.Color + '\n')
-            f.writelines(self.SecondsChoice)
+        try:
+            with open(ConfigFile, 'w') as f:
+                f.writelines(self.Color + '\n')
+                f.writelines(self.SecondsChoice)
+        except Exception as e:
+            Log("Unable to create Config file : {}".format(e))
         root.configure(cursor='none')
         self.config_window.destroy()
 
     def click_retour(self):
         # This function closes the config window
-        with open(ConfigFile, 'r') as f:
-                lines = f.read().splitlines()
-                self.Color = lines[0]
+        Log("User closed config panel with 'Retour' button")
+        try:
+            with open(ConfigFile, 'r') as f:
+                    lines = f.read().splitlines()
+                    self.Color = lines[0]
+        except Exception as e:
+            Log("Unable to read Config file while closing config panel : {}".format(e))
+            self.Color = 'red'
         self.ColorChoice = self.Color
+        root.configure(cursor='none')
         self.config_window.destroy()
+
+    def Quit(self):
+        Log("Application exited by user")
+        root.destroy()
 
     def Tick(self):
         # This function checks for time and launch ntp function every 300ms
@@ -197,14 +236,20 @@ en cours...'''
         # This function checks for network status
         try:
             # connect to the host -- tells us if the host is actually reachable
-            socket.create_connection(("www.google.com", 80), timeout=20)
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(20)
+            try:
+                sock.connect(("itransfert.francetv.fr", 80))
+            except Exception as e:
+                Log("Unable to check Network with itransfert.francetv.fr, trying with Google...  - {}".format(e))
+                sock.connect(("google.fr", 80))
             self.net_status.configure(text='Statut Réseau : OK', fg='green')
-            self.net = 'OK'
             return True
-        except:
+        except Exception as e:
+            Log("Warning - NO network connectivity : {}".format(e))
             self.net_status.configure(text='Statut Réseau : Hors ligne', fg='red')
             return False
-    
+        
     def NTP_check(self):
         # This function checks for NTP sync (works only with a raspberry for now)
         try:
@@ -220,31 +265,38 @@ en cours...'''
                         elif re.search(r'NTP synchronized: (.*)', line, re.I):
                             self.ntp_sync = re.search(r'NTP synchronized: (.*)', line, re.I)
                             n_s = self.ntp_sync.group(1).lower()
-                    if n_t == 'yes' and n_s == 'yes' and self.net == 'OK':
+                    if n_t == 'yes' and n_s == 'yes':
                         self.ntp_status.configure(text='Sync NTP : OK', fg='green')
                         return True
                     else:
+                        Log("Warning - NO NTP sync")
+                        Log("Command ouput : {}".format(out))
                         self.ntp_status.configure(text='Sync NTP : Free Run', fg='red')
                         return False
                 else:
-                    print("Unable to check NTP status.")
+                    Log("Warning - Unable to check NTP status : {}".format(err))
                     return False
             else:
-                print("Unable to check NTP because not on linux system.")
+                Log("Warning - Unable to check NTP because not on linux system.")
                 self.ntp_status.configure(text='Sync NTP : Non vérifiable', fg='gray')
-                return False
+                return True
         except Exception as e:
-            print("Error during NTP check action : {}").format(e)
+            Log("Error while checking for NTP sync : {}".format(e))
             return False
 
     def Loop(self):
         # This function calls other function on a given interval
         if self.net_check() is True and self.NTP_check() is True:
             self.clock_enable = True
+        elif self.net_check() is False and self.NTP_check() is False:
+            Log("Critical - NO NTP sync and NO network connectivity !")
         self.net_status.after(10000, self.Loop)
 
 # Start the main app
 if __name__ == '__main__':
+    Log('This is RaspiClock - {}'.format(version_text))
+    Log('Starting app now')
+    Log('Logs are stored in : {}'.format(logs_file))
     root = tk.Tk()
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
